7a0a865f954b879b3f1a7244cbe16c65e931a855
Carmencita <ccitamr@gmail.com>
2025-01-25 14:22:28 +0100

    maquetación formulario registro con array visible

--- front/crm-restaurant/src/app/app.component.ts
+++ front/crm-restaurant/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MainComponent } from './main/main.component';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -13,4 +12,30 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   title = 'crm-restaurant';
+
+
+  //configuración de la visivilidad del componente header y footer. En el componente home no deberán mostrarse pero en el resto de rutas sí.
+  private router: Router = inject(Router);
+  public showHeader: boolean = true;
+  public showFooter: boolean = true; 
+
+  ngOnInit(){
+    this.router.events.subscribe(( event ) => {
+
+      if(event instanceof NavigationEnd){
+
+        const currentRouter = event.urlAfterRedirects;
+        if(currentRouter === '/auth/profile'){ //pendiente cambiar por '/home ' cuando el componente esté creado.
+          this.showHeader = false;
+          this.showFooter = false;
+        }else {
+          this.showHeader = true;
+          this.showFooter = true;
+        }
+
+      }
+
+    })
+
+  }
 }
