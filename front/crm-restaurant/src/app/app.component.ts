f8313338332e5769ddbac51c748e96aacf4eac07
@carmen.romero <carmen.romero@sygris.com>
2025-01-30 14:01:03 +0100

    transform price

--- front/crm-restaurant/src/app/app.component.ts
+++ front/crm-restaurant/src/app/app.component.ts
@@ -2,19 +2,25 @@ import { Component, inject, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
+registerLocaleData(localeEs, 'es-ES');
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [
+    { provide: 'LOCALE_ID', useValue: 'es-ES' }
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crm-restaurant';
 
-
-  //configuración de la visivilidad del componente header y footer. En el componente home no deberán mostrarse pero en el resto de rutas sí.
+  // Configuración de la visibilidad del header y footer en función de la ruta
   private router: Router = inject(Router);
   public showHeader: boolean = true;
   public showFooter: boolean = true;
@@ -24,7 +30,7 @@ export class AppComponent {
       if (event instanceof NavigationEnd) {
         const currentRouter = event.urlAfterRedirects;
         console.log('Ruta después del redireccionamiento:', currentRouter); // Verifica la URL después de redirigir
-  
+
         if (currentRouter.startsWith('/home')) {
           this.showHeader = false;
           this.showFooter = false;
